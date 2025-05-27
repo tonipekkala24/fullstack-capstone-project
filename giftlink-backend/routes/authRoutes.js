79de67b6696572765a1b7d21c92a7b2329aebf3f
tonipekkala24 <tpekkala24@students.oamk.fi>
2025-05-27 20:43:13 +0300

    created back-end integration for login

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -55,4 +55,45 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        // Task 4: Check if the password matches the encrypyted password and send appropriate message on mismatch
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+
+            // Task 5: Fetch user details from database
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            const authtoken = jwt.sign(payload, JWT_SECRET)
+            logger.info('User logged in successfully');
+            return res.status(200).json({authtoken, userName, userEmail });
+        } else {
+            // Task 7: Send appropriate message if user not found
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
